e1e450745032e2f045bb462d006e6461e3c51179
Claudio Sumburane <claudiosumburane@gmail.com>
2019-11-21 05:03:13 +0200

    Create pais.model.ts

---
+++ src/app/models/pais.model.ts
@@ -0,0 +1,26 @@
+export class Pais {
+    name: string;
+    opLevelDomain: string[];
+    alpha2Code: string;
+    alpha3Code: string;
+    callingCodes: string[];
+    capital: string;
+    altSpellings: string[];
+    region: string;
+    subregion: string;
+    population: number;
+    latlng: number[];
+    demonym: string;
+    area: string;
+    geni: number;
+    timezones: string;
+    borders: string[];
+    nativeName: string;
+    numericCode: number;
+    currencies: object[];
+    languages: object[];
+    translations: object;
+    flag: string;
+    regionalBlocs: object[];
+    cioc: string;
+}
